d5ea9429ffd8c39c9fa6837c82f2e088f0e5a369
Dhanam08 <48279031+Dhanam08@users.noreply.github.com>
2019-08-14 11:34:37 +0530

    Add files via upload
    Openweather API used to find current temperature

---
+++ openweather.py
@@ -0,0 +1,8 @@
+import requests
+#import json
+city=input("City Name:")
+url="http://api.openweathermap.org/data/2.5/weather?q={}&appid=1da35741c11797f0de2c86ee06bdf01d".format(city)
+data=requests.get(url)
+res=data.json()
+tempature=res['main']
+print(tempature)
