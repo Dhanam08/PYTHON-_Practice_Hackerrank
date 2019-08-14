530766233bdb7236b7b523cf63756d0d5d4a3064
Dhanam08 <48279031+Dhanam08@users.noreply.github.com>
2019-08-14 18:58:30 +0530

    Update openweather.py
    upload the new file

--- openweather.py
+++ openweather.py
@@ -1,5 +1,5 @@
+#Using OpenWeather App to find Current Temperature
 import requests
-#import json
 city=input("City Name:")
 url="http://api.openweathermap.org/data/2.5/weather?q={}&appid=1da35741c11797f0de2c86ee06bdf01d".format(city)
 data=requests.get(url)
